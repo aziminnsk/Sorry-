4e034443e0e5feae10daa8adac7229d42c0ef6ae
aziminnsk <aziminsk@uvm.edu>
2016-04-18 14:39:59 -0400

    Commented
    Added comments to CPU and Player

--- CPU.java
+++ CPU.java
@@ -1,16 +1,21 @@
 import java.util.*;
 
+/* This class is used to control the CPU */
+
 public class CPU
 {
    int color;
    String name;
    int inSafe;
+
+/* Create peg objects to be used by CPU */
    
    Peg cpuPeg1 = new Peg();
    Peg cpuPeg2 = new Peg();
    Peg cpuPeg3 = new Peg();
    Peg cpuPeg4 = new Peg();
 
+/* Create CPU and set color and name and give pegs a color*/
    
    public CPU()
    {
@@ -20,6 +25,8 @@ public class CPU
       inSafe = 0;
    }
 
+/* create pegs function used to give pegs color */
+
    public void createPegs(int pColor)
    {
       cpuPeg1.setColor(pcolor);
@@ -28,26 +35,35 @@ public class CPU
       cpuPeg4.setColor(pcolor);
    }
    
+/* Set color function for necessity */
       
    public void setColor(int colorNum)
    {
       color = colorNum;
    }
+
+/* Set name function for necessity */
    
    public void setName(String pName)
    {
       name = pName;
    }
+
+/* get color function for necessity */
    
    public int getColor()
    {
       return color;
+
    }
-   
+/* get color function for necessity */   
+
    public String getName()
    {
       return name;
    }
+
+/* Method used to move a peg. Peg is chosen then moved. */
    
    public void move(int peg, int toMove)
    {
--- Player.java
+++ Player.java
@@ -1,4 +1,7 @@
+/* Alex Ziminski */
 import java.util.*;
+
+/* This class is used to control the player */
 
 public class Player
 {
@@ -6,11 +9,14 @@ public class Player
    String name;
    int inSafe;
    
+/* Create peg objects to be used by player */
+   
    Peg peg1 = new Peg();
    Peg peg2 = new Peg();
    Peg peg3 = new Peg();
    Peg peg4 = new Peg();
 
+/* Create player and set color and name and give pegs a color*/
    
    public Player(int setColor, String pName)
    {
@@ -20,6 +26,8 @@ public class Player
       inSafe = 0;
    }
 
+/* create pegs function used to give pegs color */
+
    public void createPegs(int pColor)
    {
       peg1.setColor(pcolor);
@@ -28,26 +36,35 @@ public class Player
       peg4.setColor(pcolor);
    }
    
+/* Set color function for necessity */
       
    public void setColor(int colorNum)
    {
       color = colorNum;
    }
    
+/* Set name function for necessity */
+   
    public void setName(String pName)
    {
       name = pName;
    }
+
+/* get color function for necessity */
    
    public int getColor()
    {
       return color;
    }
+
+/* get color function for necessity */
    
    public String getName()
    {
       return name;
    }
+
+/* Method used to move a peg. Peg is chosen then moved. */
    
    public void move(int peg, int toMove)
    {
